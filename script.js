21c59579d19c5396d417bbf43cf625ae10a3b28d
BelistarE <beli.estefano@gmail.com>
2024-06-20 09:07:55 -0700

    added updateDisplay()

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
+//buttons
+const addition = document.querySelector('add');
+
+const numbers = document.querySelectorAll('number');
+
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+let displayNum = "";
+let prevNum = "";
+let store = "";
+
+
+//main logic
+//when clicking a number
+numbers.forEach(number => {
+  number.addEventListener('click', handleNumberClick);
+});
+
+function handleNumberClick(event) {
+  const clickedNumber = event.target.textContent;
+
+  if (operator === "") {
+    // If no operator is selected, append to the first number
+    firstNumber += clickedNumber;
+    displayNum = firstNumber;
+  } else {
+    // If an operator is selected, append to the second number
+    secondNumber += clickedNumber;
+    displayNum = secondNumber;
+  }
+}
+
+//TODO when clicking an operator
+
+//TODO when clicking enter
+
+//TODO when clikcing c
+
+//TODO when clicking CD
+
+//update display
+function updateDisplay(value) {
+  const display = document.querySelector('.display'); // Adjust selector to match your HTML
+  display.textContent = value;
+}
+
 
 //operators
-
 function add(a , b){
   return a+b;
 }
@@ -20,11 +66,6 @@ function divide(a, b){
   return a/b;
 }
 
-let firstNumber;
-let operator;
-let secondNumber;
-let displayNum;
-let prevNum;
 
 
 function operate(operator, a, b){
